23c5acdf96baa4d00387666c5cd5c99adea7a424
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-05 14:51:43 +0100

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toggleswitch/main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,33 +18,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Hello")
-          ],
-        ),
-      ),
-    );
-  }
-}
