de94c9ef28d6f136d30deb58fc192999f67e5827
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-07 16:49:23 +0100

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:toggleswitch/consts.dart';
-import 'package:toggleswitch/myImages.dart';
-
-void main() {
-  runApp(MyApp());
-}
+import 'package:toggleswitch/homePage.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -15,76 +10,26 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool _isDarkMode = false;
 
+  // Function to toggle dark mode
+  void toggleDarkMode() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: Theme(),
-      home: CupertinoPageScaffold(
-        child: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                _isDarkMode = !_isDarkMode;
-              });
-            },
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              width: 140,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                color: _isDarkMode ? myBlack : myGrey,
-              ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  AnimatedAlign(
-                    duration: Duration(milliseconds: 300),
-                    alignment:
-                    _isDarkMode ? Alignment.centerRight : Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Container(
-                        width: 50,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: _isDarkMode ? Colors.white : Colors.black
-                        ),
-                        child: Center(
-                          child: TweenAnimationBuilder(
-                            tween: Tween(begin: 0.0, end: 1.0),
-                            duration: Duration(seconds: 9),
-                            builder: (context, value, child) {
-                              return Center(
-                                child: Image.asset(
-                                    _isDarkMode ? IconImages.blackWhite : IconImages.whiteBlack
-                                ),
-                                // child: value < 1 ? lockIcon : openLock,
-                              );
-                            },
-                            // builder: ,
-                            // child: Image.asset(
-                            //   _isDarkMode ? "images/blackWhite.png" : "images/whiteBlack.png"
-                            // ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: themeData(),
+      home: homePage(isDarkMode: _isDarkMode, toggleDarkMode: toggleDarkMode,),
     );
   }
 
-  ThemeData Theme() {
+  ThemeData themeData() {
     return ThemeData(
-      brightness: _isDarkMode ? Brightness.dark : Brightness.light
+        brightness: _isDarkMode ? Brightness.dark : Brightness.light
     );
   }
 }
+
